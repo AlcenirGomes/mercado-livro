38bdd2be62e4baa2735a3bcfde58af02755c95d5
Alcenir Gomes da Costa <alcenir.g.costa@hotmail.com>
2025-02-15 13:56:46 -0300

    Update of the Mercado Livro project... Update and connection implemented from Database

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 
 group = "com.mercadolivro"
 version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 java {
 	toolchain {
@@ -16,12 +17,18 @@ java {
 
 repositories {
 	mavenCentral()
+	mavenLocal()
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("com.mysql:mysql-connector-j:8.0.33")
+
+	runtimeOnly("mysql:mysql-connector-java")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- src/main/kotlin/com/mercadolivro/service/CustomerService.kt
+++ src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -10,14 +10,15 @@ class CustomerService {
 
     fun getAll(name: String?): List<CustomerModel> {
         name?.let {
-            return customers.filter { it.name.contains(name, ignoreCase = true) }
+            return customers.filter { it.name.contains(name, true) }
         }
         return customers
     }
+
     fun create(customer: CustomerModel) {
         val id = if(customers.isEmpty()) {
             1
-        } else {
+        } else{
             customers.last().id!!.toInt() + 1
         }.toString()
 
@@ -25,16 +26,20 @@ class CustomerService {
 
         customers.add(customer)
     }
+
     fun getCustomer(id: String): CustomerModel {
         return customers.filter { it.id == id }.first()
     }
+
     fun update(customer: CustomerModel) {
         customers.filter { it.id == customer.id }.first().let {
             it.name = customer.name
             it.email = customer.email
         }
     }
+
     fun delete(id: String) {
-        customers.removeIf{ it.id == id}
+        customers.removeIf { it.id == id }
     }
+
 }
